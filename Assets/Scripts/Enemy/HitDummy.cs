999e65f1e9dd0b2b222150b3b4db440296a9067a
kordiway <158851473+kordiway@users.noreply.github.com>
2026-05-24 13:32:45 -0600

    Added hit dummy

---
+++ Assets/Scripts/Enemy/HitDummy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TMPro;
+public class HitDummy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject damagePopupPrefab;
+
+public void TakeHit(int damage)
+    {
+        GameObject popup = Instantiate(damagePopupPrefab, transform.position, Quaternion.identity);
+        popup.GetComponent<DamagePopup>().SetDamageText(damage);
+    }
+}
